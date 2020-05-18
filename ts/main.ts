f3c1b6248c9ab54b6cb0c6be9fc7d22a85bbf9bb
Joseph Ortiz <joeprogrammer88@users.noreply.github.com>
2020-05-18 16:01:37 -0700

    Saving ToDoItem is functional

--- ts/main.ts
+++ ts/main.ts
@@ -17,12 +17,20 @@ item.isCompleted = false;
 window.onload = function(){
     let addItem = document.getElementById("add");
     addItem.onclick = main;
+
+    loadSavedItem();
+}
+
+function loadSavedItem(){
+    let item = getToDo(); // read from storage
+    displayToDoItem(item);
 }
 
 function main(){
     if(isValid()){
         let item = getToDoItem();
         displayToDoItem(item);
+        saveToDo(item);
     }
 }
 
@@ -68,7 +76,9 @@ function displayToDoItem(item:ToDoItem):void{
 
     // ex. <p>June 1st 2020</p>
     let itemDate = document.createElement("p");
-    itemDate.innerText = item.dueDate.toDateString();
+    // itemDate.innerText = item.dueDate.toDateString();
+    let dueDate = new Date(item.dueDate.toString());
+    itemDate.innerText = dueDate.toDateString();
 
     // ex. <div class="todo completed"></div> or <div class="todo"></div>
     let itemDiv = document.createElement("div");
@@ -109,3 +119,23 @@ function markAsComplete(){
 }
 
 // Task: Store ToDoItems in web storage
+
+function saveToDo(item:ToDoItem):void{
+    // Convert ToDoItem into JSON string
+    let itemString = JSON.stringify(item);
+
+    // Save string
+    localStorage.setItem(todokey, itemString);
+}
+
+const todokey = "todo";
+
+/**
+ * Get stored ToDo item or return null if
+ * none is found
+ */
+function getToDo():ToDoItem{
+    let itemString = localStorage.getItem(todokey);
+    let item:ToDoItem = JSON.parse(itemString);
+    return item;
+}
